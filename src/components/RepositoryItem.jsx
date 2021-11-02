e7b08929f67176b213e95465ca472ff43cad08cf
felipeduartedeveloper <felipesilvapac@gmail.com>
2021-11-02 00:06:35 -0300

    chore: init typescript config

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,6 +3,9 @@ export function RepositoryItem(props) {
     <li>
           <strong>{props.repository.name}</strong>
           <p>{props.repository.description}</p>
+
+          
+
           <a href={props.repository.html_url}>
             Acessar repositório
           </a>
